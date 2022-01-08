aceff3e11c42cf32f764564b52d8230460fdfb05
İsmail Sezer Bullalıoğlu <bullalioglu@gmail.com>
2022-01-08 05:23:05 +0300

    Update CSS. Delete unused files. Clear unused code. Add cross out item feature. Add clear list feature.

--- app.js
+++ app.js
@@ -18,18 +18,6 @@ app.get("/", function(req, res) {
     // new Date() -with no arguments- returns the current date.
     const currentDate = new Date();
 
-    /*
-    const dayOfWeek = currentDate.getDay(); // Sun: 0, ... Sat: 6
-    const dayOfMonth = currentDate.getDate();
-    const currentMonth = currentDate.getMonth(); // Jan: 0, ... Dec: 11
-    const currentYear = currentDate.getFullYear();
-
-    const currentDayName = dayName(dayOfWeek);
-    const currentMonthName = monthName(currentMonth);
-
-    const currentDateText = currentMonthName + " " + dayOfMonth + ", " + currentYear;
-    */
-
     let options = {
         // weekday: 'long',
         year: 'numeric',
@@ -38,16 +26,23 @@ app.get("/", function(req, res) {
     }
 
     // currentDate.toLocaleString() = currentDate.toLocaleDateString() + currentDate.toLocaleTimeString()
-    const currentDateText = currentDate.toLocaleDateString("en-US", options);
-    const currentDayName = currentDate.toLocaleDateString("en-US", {weekday: 'long'});
+    const currentDateText = currentDate.toLocaleDateString([], options);
+    const currentDayName = currentDate.toLocaleDateString([], {weekday: 'long'});
     // Pass [] into locales for the default location.
 
     res.render("list", {date: currentDateText, day: currentDayName, todo: toDoList});
-    // res.sendFile(__dirname + "/index.html");
 })
 
 app.post("/", function(req, res) {
-    toDoList.push(req.body.newItem);
+
+    let newListItem = req.body.newItem;
+
+    // Check if empty string, null, or all whitespace before pushing into array.
+    if(newListItem === "*clear*") {
+        toDoList = [];
+    } else if(!!newListItem && !!newListItem.trim()) {
+        toDoList.push(req.body.newItem);
+    }
 
     res.redirect("/");
 })
@@ -58,72 +53,3 @@ const port = 3000;
 app.listen(port, function() {
     console.log("Server running on port " + port);
 })
-
-/*
-function dayName(dayNumber) {
-    switch (dayNumber) {
-        case 0:
-            return "Sunday";
-            break;
-        case 1:
-            return "Monday";
-            break;
-        case 2:
-            return "Tuesday";
-            break;
-        case 3:
-            return "Wednesday";
-            break;
-        case 4:
-            return "Thursday";
-            break;
-        case 5:
-            return "Friday";
-            break;
-        case 6:
-            return "Saturday";
-            break;
-    }
-}
-
-function monthName(monthNumber) {
-    switch (monthNumber) {
-        case 0:
-            return "January";
-            break;
-        case 1:
-            return "February";
-            break;
-        case 2:
-            return "March";
-            break;
-        case 3:
-            return "April";
-            break;
-        case 4:
-            return "May";
-            break;
-        case 5:
-            return "June";
-            break;
-        case 6:
-            return "July";
-            break;
-        case 7:
-            return "August";
-            break;
-        case 8:
-            return "September";
-            break;
-        case 9:
-            return "October";
-            break;
-        case 10:
-            return "November";
-            break;
-        case 11:
-            return "December";
-            break;
-    }
-}
-*/
